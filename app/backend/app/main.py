98b3a433c8003f6a19a53b51f3bda5be048b8599
Gnoah0296 <tnh.hoang0296@gmail.com>
2025-12-28 23:13:23 +0700

    Add files via upload

---
+++ app/backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .db import engine
+from .models import Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message": "Fusion system is running"}
